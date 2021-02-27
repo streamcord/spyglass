b2d9c887b657f95d9e1a753116f46008dacbda14
Campbell Jones <git@serebit.com>
2021-02-27 11:39:14 -0500

    Update deps, use Kotlin language version 1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.30"
-    kotlin("plugin.serialization") version "1.4.30"
+    kotlin("jvm") version "1.4.31"
+    kotlin("plugin.serialization") version "1.4.31"
     id("com.github.ben-manes.versions") version "0.36.0"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     application
@@ -16,15 +16,15 @@ repositories {
 
 dependencies {
     // HTTP Server
-    implementation("io.ktor", "ktor-server-netty", "1.5.1")
+    implementation("io.ktor", "ktor-server-netty", "1.5.2")
     implementation("org.slf4j", "slf4j-simple", "2.0.0-alpha1")
 
     // HTTP Client
-    implementation("io.ktor", "ktor-client-java", "1.5.1")
+    implementation("io.ktor", "ktor-client-java", "1.5.2")
 
     // Serialization
     implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.1.0")
-    implementation("com.charleskorn.kaml", "kaml", "0.27.0")
+    implementation("com.charleskorn.kaml", "kaml", "0.28.3")
 
     // DB
     implementation("org.mongodb", "mongodb-driver", "3.12.8")
@@ -41,6 +41,7 @@ tasks {
     withType<KotlinCompile>().configureEach {
         kotlinOptions.jvmTarget = "11" // require Java 11 so we can use the built-in HttpClient
         kotlinOptions.useIR = true
+        kotlinOptions.languageVersion = "1.5" // for sealed interfaces and value classes
         kotlinOptions.freeCompilerArgs = listOf("-Xinline-classes")
     }
 
