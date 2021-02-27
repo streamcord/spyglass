82145fb70df2160c208ec0947b6565ef8abce940
Campbell Jones <git@serebit.com>
2021-02-27 11:39:54 -0500

    Enable and flesh out AQMP sender

--- src/main/kotlin/AqmpSender.kt
+++ src/main/kotlin/AqmpSender.kt
@@ -2,10 +2,19 @@ package io.streamcord.webhooks.server
 
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.ConnectionFactory
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.io.IOException
 
 class AqmpSender private constructor(private val queueName: String, private val channel: Channel) {
-    fun send(text: String) = channel.basicPublish("", queueName, null, text.encodeToByteArray())
+    fun sendOnlineEvent(streamID: String, userID: String) = send(AqmpEvent.StreamOnline(streamID, userID))
+
+    fun sendOfflineEvent(userID: String) = send(AqmpEvent.StreamOffline(userID))
+
+    private inline fun <reified T> send(obj: T) = send(Json.encodeToString(obj))
+
+    private fun send(text: String) = channel.basicPublish("", queueName, null, text.encodeToByteArray())
 
     companion object {
         fun create(config: AppConfig.Aqmp): AqmpSender? = try {
@@ -23,5 +32,19 @@ class AqmpSender private constructor(private val queueName: String, private val 
             System.err.println("Failed to establish AQMP queue due to ${ex.stackTraceToString()}")
             null
         }
+    }
+}
+
+private sealed interface AqmpEvent {
+    val op: Int
+
+    @Serializable
+    data class StreamOnline(val streamId: String, val userID: String) : AqmpEvent {
+        override val op = 1
+    }
+
+    @Serializable
+    data class StreamOffline(val userId: String) : AqmpEvent {
+        override val op = 2
     }
 }
--- src/main/kotlin/TwitchServer.kt
+++ src/main/kotlin/TwitchServer.kt
@@ -6,12 +6,15 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.util.pipeline.*
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.decodeFromJsonElement
 
 fun createHttpServer(twitchClient: TwitchClient, aqmpConfig: AppConfig.Aqmp) {
-    // val aqmpSender = AqmpSender.create(aqmpConfig) ?: return null
+    val aqmpSender = AqmpSender.create(aqmpConfig) ?: error("Failed to open AQMP sender")
 
     embeddedServer(Netty, port = 8080) {
         routing {
@@ -24,11 +27,54 @@ fun createHttpServer(twitchClient: TwitchClient, aqmpConfig: AppConfig.Aqmp) {
                         call.respond(verificationBody.challenge)
                         twitchClient.verifyCallback()
                     }
-                    "notification" -> println(text)
+                    "notification" -> handleNotification(aqmpSender, text)
                 }
             }
         }
     }.start(wait = true)
+}
+
+private fun PipelineContext<Unit, ApplicationCall>.handleNotification(sender: AqmpSender, text: String) {
+    val notification = Json.decodeFromString<TwitchNotification>(text)
+
+    when (val subType = call.request.header("Twitch-Eventsub-Subscription-Type")) {
+        "stream.online" -> {
+            val eventData = Json.decodeFromJsonElement<TwitchEvent.StreamOnline>(notification.event)
+
+            if (eventData.type == "live") {
+                sender.sendOnlineEvent(eventData.id, eventData.broadcaster_user_id)
+            }
+        }
+        "stream.offline" -> {
+            val eventData = Json.decodeFromJsonElement<TwitchEvent.StreamOffline>(notification.event)
+            sender.sendOfflineEvent(eventData.broadcaster_user_id)
+        }
+        else -> {
+            System.err.println("Received notification for subscription type $subType, ignoring")
+        }
+    }
+}
+
+@Serializable
+private data class TwitchNotification(val subscription: SubscriptionData, val event: JsonElement)
+
+private interface TwitchEvent {
+    @Serializable
+    data class StreamOnline(
+        val id: String,
+        val broadcaster_user_id: String,
+        val broadcaster_user_login: String,
+        val broadcaster_user_name: String,
+        val type: String,
+        val started_at: String
+    ) : TwitchEvent
+
+    @Serializable
+    data class StreamOffline(
+        val broadcaster_user_id: String,
+        val broadcaster_user_login: String,
+        val broadcaster_user_name: String
+    )
 }
 
 @Serializable
