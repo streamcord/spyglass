04c435fb9ff9aa5c2a94a96df402e746019ee6a7
Campbell Jones <git@serebit.com>
2021-03-20 19:43:23 -0400

    Add exit code reference object

---
+++ src/main/kotlin/ExitCodes.kt
@@ -0,0 +1,10 @@
+package io.streamcord.spyglass
+
+object ExitCodes {
+    const val MISSING_ENV_VAR = 1
+    const val MISSING_CONFIG_FILE = 2
+    const val NO_TWITCH_ACCESS_TOKEN = 3
+    const val INVALID_DB_CONNECTION_STRING = 4
+    const val INVALID_DB_NAME = 5
+    const val INVALID_DB_COLLECTION_NAME = 6
+}
