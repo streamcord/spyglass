fb5005197db62265575b034519f9ee380e216b5d
Campbell Jones <git@serebit.com>
2021-04-05 18:57:47 -0400

    Merge Database.kt and DatabaseController.kt

--- src/main/kotlin/Database.kt
+++ src/main/kotlin/Database.kt
@@ -1,6 +1,8 @@
 package io.streamcord.spyglass
 
+import com.mongodb.client.MongoClients
 import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
 import com.mongodb.client.model.Aggregates
 import com.mongodb.client.model.Filters
 import com.mongodb.client.model.changestream.ChangeStreamDocument
@@ -9,6 +11,42 @@ import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onEach
 import org.bson.Document
+
+class DatabaseController private constructor(
+    val subscriptions: MongoCollection<Document>,
+    val notifications: MongoCollection<Document>,
+) {
+    companion object {
+        fun create(config: AppConfig.Mongo): DatabaseController {
+            val client = try {
+                MongoClients.create("mongodb://${config.connection}")
+            } catch (ex: IllegalArgumentException) {
+                logger.fatal(
+                    ExitCodes.INVALID_DB_CONNECTION_STRING,
+                    "Invalid MongoDB connection string ${config.connection}",
+                    ex
+                )
+            }
+
+            val database = try {
+                client.getDatabase(config.database)
+            } catch (ex: IllegalArgumentException) {
+                logger.fatal(ExitCodes.INVALID_DB_NAME, "Invalid MongoDB database name ${config.database}", ex)
+            }
+
+            val subscriptions = database.tryGetCollection(config.collections.subscriptions)
+            val notifications = database.tryGetCollection(config.collections.notifications)
+
+            return DatabaseController(subscriptions, notifications)
+        }
+
+        private fun MongoDatabase.tryGetCollection(collectionName: String) = try {
+            getCollection(collectionName)
+        } catch (ex: IllegalArgumentException) {
+            logger.fatal(ExitCodes.INVALID_DB_COLLECTION_NAME, "Invalid MongoDB collection name $collectionName", ex)
+        }
+    }
+}
 
 fun MongoCollection<Document>.insertSubscription(clientID: ClientID, secret: String, data: SubscriptionData) {
     insertOne(document {
